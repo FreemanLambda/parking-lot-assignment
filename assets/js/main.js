09f64a349a98c154a8b9011e6b59841ec2600ea4
Mario Banushi <mariobanushi@hotmail.com>
2016-11-09 16:53:52 +0100

    smoother visuals, responsive design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-var myChart = null, timeline = [], lotUrl = 'http://parkingapi.gear.host/v1/parking';
+var myChart = null,
+		timeline = [],
+		lotUrl = 'http://parkingapi.gear.host/v1/parking';
 
 function toTimestamp(minutes) {
 	var hours = Math.floor(minutes / 60);
@@ -17,22 +19,7 @@ function incrementTimestamp(ts, minutes) {
 	return toTimestamp(toMinutes(ts) + parseInt(minutes));
 }
 
-function drawChart(start, stop) {
-  var timelineFragment = timeline.filter(function(nrCars, index) {
-    var ts = toTimestamp(index);
-    return ts >= start && ts <= stop;
-  });
-
-  // show max nr of cars result
-  var results = {
-    maxCars: 0,
-    maxTime: '00:00'
-  }
-  results = timelineFragment.reduce(function(oldResults, nrCars, time) {
-    return (nrCars > oldResults.maxCars)  ? { maxCars: nrCars, maxTime: incrementTimestamp(start, time) } : oldResults;
-  }, results);
-  $('#results').html('Max cars: ' + results.maxCars + ' at: ' + results.maxTime);
-
+function drawChart(start, timelineFragment) {
   // destroy old chart (if any) and draw new chart
   var ctx = $('#timeline-chart');
   if(myChart) {
@@ -60,14 +47,34 @@ function drawChart(start, stop) {
   });
 }
 
+function updateResults(start, stop) {
+	var timelineFragment = timeline.filter(function(nrCars, index) {
+		var ts = toTimestamp(index);
+		return ts >= start && ts <= stop;
+	});
+
+	// show max nr of cars result
+	var results = {
+		maxCars: 0,
+		maxTime: '00:00'
+	}
+	results = timelineFragment.reduce(function(oldResults, nrCars, time) {
+		return (nrCars > oldResults.maxCars)  ? { maxCars: nrCars, maxTime: incrementTimestamp(start, time) } : oldResults;
+	}, results);
+	$('#results').html('Max cars: ' + results.maxCars + ' at: ' + results.maxTime);
+	drawChart(start, timelineFragment);
+}
+
 $(document).ready(function() {
+	// show empty results, for aesthetics
+	updateResults('00:00', '23:59');
   // get data modal
   var $apiInfo = $('.api-info');
   $('#get-data').on('click', function() {
     var nrDays = $('#nr-days').val();
     var nrRecords = $('#nr-records').val();
 
-    $apiInfo.html('<i class="fa fa-spinner fa-spin"></i>')
+    $apiInfo.html('Fetching data <i class="fa fa-spinner fa-spin"></i>')
     var timelineWorker = new Worker('/timelineWorker.js');
     timelineWorker.addEventListener('message', function(event) {
       if(event.data.err) {
@@ -76,6 +83,10 @@ $(document).ready(function() {
       else {
         $apiInfo.html('Successfully loaded data as requested.');
         timeline = JSON.parse(event.data);
+				updateResults('00:00', '23:59');
+				setTimeout(function() {
+					$('#get-data-modal').modal('hide');
+				}, 1500)
       }
     });
     timelineWorker.postMessage({url: lotUrl, nrDays: nrDays, nrRecords: nrRecords});
@@ -103,12 +114,12 @@ $(document).ready(function() {
   $('#narrow-timeline').on('click', function() {
     var start = toTimestamp(toMinutes($('#start-time').val()));
     var stop = toTimestamp(toMinutes($('#end-time').val()));
-    drawChart(start, stop);
+    updateResults(start, stop);
   });
   $('#all-time').on('click', function(e) {
     e.preventDefault();
     $('#start-time').val('00:00');
     $('#end-time').val('23:59');
-    $('#narrow-timeline').click();
+    updateResults('00:00', '23:59');
   });
 });
